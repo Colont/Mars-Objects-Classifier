9b88d1e2cd6af8c2f19543f4abddb78984ac3d1e
Colin Johnson <colinjohnsoon@gmail.com>
2025-01-27 19:44:00 -0500

    Preprocessing Images

--- preprocessing.py
+++ preprocessing.py
@@ -6,25 +6,54 @@ import pandas as pd
 from keras.utils import to_categorical
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
+import numpy as np
 import os
 
 cwd = os.getcwd()
-
 vocab_df, train_df, test_df, val_df = main()
 
 # Identify Labels based on the terms
-train_df['Labels'] = train_df['ID'].map(vocab_df.set_index('ID')['Term'])
-test_df['Labels'] = test_df['ID'].map(vocab_df.set_index('ID')['Term'])
 
 # Add normalized image values to a matrix
-normalized_images = []
-for url in train_df['Picture']:
-    img = load_img(cwd + '/msl-images/' + url)
-    img_array = img_to_array(img)
+def normalization(train_df, test_df, vocab_df):
+    '''
+    Normalize the pixel values of the images into a matrix of values
+    Param: Train_df (training dataset)
+    Param: Test_df (testing dataset)
+    Param: Vocab_df (vocab dataset)
+    '''
+    # Map the labels from each df to the vocab name in the vocab df
+    train_labels = train_df['ID'].map(vocab_df.set_index('ID')['Term'])
+    test_labels = test_df['ID'].map(vocab_df.set_index('ID')['Term'])
     
-    # Normalize pixel values
-    img_array_normalized = img_array / 255.0
+    train_normalized = []
+    test_normalized = []
+    # Looks at the column in train_df of images and converts it into a pixel array
+    for url in train_df['Picture']:
+        img = load_img(cwd + '/msl-images/' + url)
+        img_array = img_to_array(img)
     
-    # Append normalized array to the list
-    normalized_images.append(img_array_normalized)
-print(normalized_images)
+        # Normalize pixel values
+        img_array_normalized = img_array / 255.0
+    
+        # Append normalized array to the list
+        train_normalized.append(img_array_normalized)
+    for url in test_df['Picture']:
+        img = load_img(cwd + '/msl-images/' + url)
+        img_array = img_to_array(img)
+    
+        # Normalize pixel values
+        img_array_normalized = img_array / 255.0
+    
+        # Append normalized array to the list
+        test_normalized.append(img_array_normalized)
+        # Dispaly a few of the training images
+        for i in range(25):
+            plt.subplot(5,5,i+1)
+            plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_normalized[i])
+        plt.xlabel(train_labels[i])
+    plt.show()
+    return train_normalized, test_normalized
